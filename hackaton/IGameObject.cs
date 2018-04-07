7cdb70e245c59e36c4ebd254608772daf6a2c8f2
Danko <wally19@bk.ru>
2018-04-07 13:15:29 +0400

    +IGameObject *Form1 +PaintData

---
+++ hackaton/IGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace hackaton
+{
+    public interface IGameObject
+    {
+        Bitmap Sprite { get; }
+        Point Position { get; }
+    }
+}
